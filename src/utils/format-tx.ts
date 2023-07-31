3e0fa1c933ad3f40db77a8a3f5f4338b00402b01
azizmamoyan <mailter@mail.ru>
2023-07-31 15:57:14 +0300

    fixes

--- src/utils/format-tx.ts
+++ src/utils/format-tx.ts
@@ -18,6 +18,7 @@ import nets from "../nets/net";
 const TransferTopic = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
 const ApprovalTopic = "0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925"
 const WithdrawWETHTopic = "0x7fcf532c15f0a6db0bd6d0e038bea71d30d808c7d98cb3bf7268a95bf5081b65"
+const DepositWETHTopic = "0xe1fffcc4923d04b559f4d29a8bfc6cda04eb5b0d3c460751c2402c5c5cc9109c"
 
 const face = {
     pancakePair : new Interface(abiPancakePair),
@@ -35,6 +36,7 @@ export function fromHex(hexAddress: string): string {
 enum Method {
     transfer = "transfer",
     withdrawWETH = "withdraw",
+    depositWETH = "deposit",
     transferFrom = "transferFrom",
     addLiquidityETH = "addLiquidityETH",
     stake = "stake",
@@ -151,6 +153,7 @@ export function formatEth(transaction: TransactionResponse): TX {
     tx.hash = transaction.hash;
     tx.from = transaction.from.toLowerCase() as Lowercase<string>;
     tx.amountIn = transaction?.value || 0;
+    tx.to=transaction.to.toLowerCase() as Lowercase<string>;
 
 
     let routerFace = face.pancakeRouterV2;
@@ -280,6 +283,13 @@ export function formatLog(log:Log):TX{
         tx.to=d.spender;
         tx.amountIn=tx.amountOut=d.value;
         tx.method=Method.approve;
+    }else 
+    if(log.topics[0]===DepositWETHTopic){
+        const d = face.weth.decodeEventLog("Deposit", log.data, log.topics)
+        tx.from = d.dst;
+        tx.to = d.dst;
+        tx.amountIn=tx.amountOut=d.wad;
+        tx.method=Method.depositWETH;
     }else 
     if(log.topics[0]===WithdrawWETHTopic){
         const d = face.weth.decodeEventLog("Withdrawal", log.data, log.topics)
