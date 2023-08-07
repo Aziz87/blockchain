afbcb004cd405a5c28f20af01287afd75d38f411
azizmamoyan <mailter@mail.ru>
2023-08-07 19:02:07 +0300

    polygon uniswap v3

--- src/nets/poligon.net.ts
+++ src/nets/poligon.net.ts
@@ -1,4 +1,4 @@
-import { Symbol, NET, NetworkName } from "./net.i";
+import { Symbol, NET, NetworkName, SwapRouterVersion } from "./net.i";
 
 const Poligon: NET = {
     id: 137,
@@ -21,8 +21,9 @@ const Poligon: NET = {
         ], symbol: Symbol.MATIC, decimals: 18
     }],
     miningBlockSeconds: 2.3,
-    wrapedNativToken: null,
+    wrapedNativToken: { address: '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270', decimals: 18, symbol: Symbol.WMATIC },
     tokens: [
+        { address: '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270', decimals: 18, symbol: Symbol.WMATIC },
         { address: '0xc2132d05d31c914a87c6611c10748aeb04b58e8f', decimals: 6, symbol: Symbol.USDT },
         { address: '0x2791bca1f2de4661ed88a30c99a7a9449aa84174', decimals: 6, symbol: Symbol.USDC },
         { address: '0xdab529f40e671a1d4bf91361c21bf9f0c9712ab7', decimals: 18, symbol: Symbol.BUSD },
@@ -30,7 +31,12 @@ const Poligon: NET = {
         { address: '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619', decimals: 18, symbol: Symbol.WETH}
     ],
     requestsPerSecond:5,
-    swapRouters:[]
+    swapRouters:[
+        {version:SwapRouterVersion.METAMASK_SWAP, address: "0x1a1ec25dc08e98e5e93f1104b5e5cdd298707d31"},
+        {version:SwapRouterVersion.UNISWAP_V3, address: "0xe592427a0aece92de3edee1f18e0157c05861564", quoters:[
+            {v:2, address:"0xe592427a0aece92de3edee1f18e0157c05861564",methodName:"uniswapV3SwapCallback",supportedFees:[10000n, 3000n, 500n, 100n]}
+        ]},
+    ]
 }
 
 export default Poligon;
